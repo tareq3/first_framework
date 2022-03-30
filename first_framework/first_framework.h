dec0c5436a0bd3e7a35a9d509e0577049840b268
tareq3 <mti.tareq3@gmail.com>
2022-03-30 23:23:09 +0600

    Initial Commit

---
+++ first_framework/first_framework.h
@@ -0,0 +1,18 @@
+//
+//  first_framework.h
+//  first_framework
+//
+//  Created by Tareq on 30/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for first_framework.
+FOUNDATION_EXPORT double first_frameworkVersionNumber;
+
+//! Project version string for first_framework.
+FOUNDATION_EXPORT const unsigned char first_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <first_framework/PublicHeader.h>
+
+
